eb94660648cc626e16792c71f9142c1d93f63695
Aman Adhav <amanadhav@gmail.com>
2018-02-03 21:31:00 -0500

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
        $.ajax({
-        url: "https://4805dc04.ngrok.io",
+        url: "https://70afc598.ngrok.io",
         type: "POST",
         data: {
           value: name
